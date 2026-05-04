25a9f94ffeeb380f43b49124bf7a7ce5d85651c7
ndhungtt151290-arch <ndhungtt151290@gmail.com>
2026-05-04 13:36:59 +0900

    update gitignore

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"79vbMtdyCDdEitlnEusTPJWN6gJB7KLrpYbhHyzUT50=\"\n}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"KCzpfRfyZd67fHcrHOnpz/2kpg44uFrwxlkyqGQD/GY=\"\n}"
